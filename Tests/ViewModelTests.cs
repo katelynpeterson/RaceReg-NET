91ad661777080bc61342c3e94ee87fe3cf3d431a
Jackson Porter <jackson_porter@outlook.com>
2018-11-12 10:24:52 -0700

    Added a test project, created a failing test.

---
+++ Tests/ViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+
+    [TestFixture]
+    public class ViewModelTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
