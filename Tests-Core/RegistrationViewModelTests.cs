f9723169a4202b8c9832ff7010ecfa9d227e6935
Jackson Porter <jackson_porter@outlook.com>
2018-11-30 15:29:05 -0700

    Creating test implemenation of Database.

--- Tests-Core/RegistrationViewModelTests.cs
+++ Tests-Core/RegistrationViewModelTests.cs
@@ -1,11 +1,6 @@
 ﻿using NUnit.Framework;
 using RaceReg.Helpers;
-using RaceReg.Model;
-using RaceReg.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Security;
-using System.Threading.Tasks;
 
 namespace Tests
 {
@@ -34,38 +29,7 @@ namespace Tests
 
         }
 
-        private class TestDB : IRaceRegDB
-        {
-            public Task<Affiliation> AddNewAffiliationAsync(Affiliation affiliation)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task<User> GrabUserDetailsAsync(string username, SecureString password)
-            {
-                throw new NotImplementedException();
-            }
-
-            public async Task<IEnumerable<Affiliation>> RefreshAffiliations()
-            {
-                return await Task.FromResult(new List<Affiliation>());
-            }
-
-            public Task<IEnumerable<Participant>> RefreshParticipants()
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task<string> Save(Participant updatedParticipant)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task<Participant> SaveParticipant(Participant updatedParticipant)
-            {
-                throw new NotImplementedException();
-            }
-        }
+        
 
         private class TestDialogService : IDialogService
         {
---
+++ Tests-Core/TestDatabase.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using RaceReg.Model;
+using RaceReg.ViewModel;
+
+namespace Tests_Core
+{
+    public class TestDatabase
+    {
+        List<Affiliation> affiliations;
+        List<Participant> participants;
+        int currentAffiliationId;
+        int currentParticipantId;
+
+        public TestDatabase()
+        {
+            affiliations = new List<Affiliation>();
+            currentAffiliationId = 1;
+            currentParticipantId = 1;
+        }
+
+        public async Task<Affiliation> AddNewAffiliationAsync(Affiliation affiliation)
+        {
+            affiliation.Id = currentAffiliationId;
+            currentAffiliationId++;
+            affiliations.Add(affiliation);
+
+            return await Task.FromResult(affiliations[affiliations.Count() - 1]);
+        }
+
+        public Task<User> GrabUserDetailsAsync(string username, SecureString password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Affiliation>> RefreshAffiliations()
+        {
+            if(affiliations.Count == 0)
+            {
+                Random rand = new Random();
+
+                for (int i = 0; i < rand.Next(1, 51); i++)
+                {
+                    Affiliation temp = new Affiliation();
+                    temp.Id = currentAffiliationId;
+                    currentAffiliationId++;
+                    temp.Name = "Affiliation " + temp.Id;
+                    temp.Abbreviation = "A" + temp.Id;
+                    affiliations.Add(temp);
+                }
+            }
+
+            return await Task.FromResult(affiliations);
+        }
+
+        public Task<IEnumerable<Participant>> RefreshParticipants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Save(Participant updatedParticipant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Participant> SaveParticipantAsync(Participant updatedParticipant)
+        {
+            int index = -1;
+            for(int i = 0;  i < participants.Count(); i++)
+            {
+                if (participants[i].Id == updatedParticipant.Id)
+                {
+                    participants[i] = updatedParticipant;
+                    index = i;
+                    break;
+                }
+            }
+
+            if(index < 0)
+            {
+                Participant toReturn = null;
+                return await Task.FromResult(toReturn);
+            }
+            else
+            {
+                return await Task.FromResult(participants[index]);
+            }
+        }
+    }
+}
