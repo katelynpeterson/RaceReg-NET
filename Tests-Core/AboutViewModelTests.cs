9443e0184d0b092b0a5efad2a86a1c07b8a967e3
Jackson Porter <jackson_porter@outlook.com>
2018-11-30 20:43:45 -0700

    Added more tests

---
+++ Tests-Core/AboutViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests_Core
+{
+    [TestFixture]
+    class AboutViewModelTests
+    {
+        [Test]
+        public void SwitchToPreviousViewTest()
+        {
+
+        }
+    }
+}
