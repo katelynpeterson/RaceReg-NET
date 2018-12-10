701033681f5819680fe8192d890a10455a175d54
Jackson Porter <jackson_porter@outlook.com>
2018-12-10 15:32:18 -0700

    Starting to add tests for addparticipantview

---
+++ Tests-Core/AddParticipantViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RaceReg.Helpers;
+using RaceReg.ViewModel;
+using System;
+using Tests_Core;
+
+namespace Tests_Core
+{
+
+    [TestFixture]
+    public class AddParticipantViewModelTests
+    {
+        [TestCase("Jackson", "Porter", 1, "Male", "1997-01-02")]
+        public void SaveNewParticipant(string firstName, string lastName, int affiliationId, string gender, string birthdate)
+        {
+
+        }
+    }
+}
