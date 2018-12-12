c84e6ac41a413d3027b0e6debe6a69fd1daa52ab
Jackson Porter <jackson_porter@outlook.com>
2018-12-12 03:44:04 -0700

    Create a meet model

---
+++ RaceReg/Model/Meet.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaceReg.Model
+{
+    class Meet : ViewModelBase
+    {
+        private int id;
+        private string name;
+        private string description;
+        private DateTime startDateTime;
+        private DateTime endDate;
+        private int userId;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public DateTime StartDateTime { get { return startDateTime; } set { startDateTime = value; } }
+        public DateTime EndDate { get { return endDate; } set { endDate = value; } }
+        public int UserId { get { return userId; } set { userId = value; } }
+
+        public Meet()
+        {
+            Id = -1;
+        }
+    }
+}
--- Tests-Core/TestDatabaseTests.cs
+++ Tests-Core/TestDatabaseTests.cs
@@ -239,5 +239,50 @@ namespace Tests_Core
         //    Assert.AreEqual(affiliations[0], newAffiliation);
 
         //}
+
+        [TestCase("Jackson", "Porter", "m", "MHS", "1997-01-01")]
+        public async Task SaveNewParticipantTest(string firstName, string lastName, string gender, string affiliationAbbreviation, string birthDate)
+        {
+            /** Make a new database **/
+            var testDB = new TestDatabase();
+
+            /** Make the participant **/
+            Participant newParticipant = new Participant();
+
+            newParticipant.FirstName = firstName;
+            newParticipant.LastName = lastName;
+
+            if (gender.Equals("m"))
+            {
+                newParticipant.Gender = Participant.GenderType.Male;
+            }
+            else if (gender.Equals("f"))
+            {
+                newParticipant.Gender = Participant.GenderType.Female;
+            }
+            else
+            {
+                newParticipant.Gender = Participant.GenderType.Other;
+            }
+
+            /** Make an affiliation with matching abbreviation and store in DB **/
+            Affiliation matchingAffiliation = new Affiliation();
+            matchingAffiliation.Abbreviation = affiliationAbbreviation;
+            await testDB.AddNewAffiliationAsync(matchingAffiliation);
+
+            /** Store participant in database **/
+            Participant returnedParticipant = await testDB.SaveNewParticipant(newParticipant);
+            newParticipant.Id = returnedParticipant.Id;
+
+            /** Forces a failed test **/
+            //Participant returnedParticipant1 = await testDB.SaveNewParticipant(new Participant());
+
+            /** Verify participant excists **/
+            var listParticipants = await testDB.RefreshParticipants();
+            ObservableCollection<Participant> participants = new ObservableCollection<Participant>(listParticipants);
+
+            /** Test Equality **/
+            Assert.AreEqual(participants[0], newParticipant);
+        }
     }
 }
